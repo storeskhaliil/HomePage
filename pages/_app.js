5e8175c9b41b4462fbb09bce0c649b3a13f2df00
storeskhaliil <storeskhaliil@gmail.com>
2025-11-09 20:11:15 +0200

    Add Footer component to the main app layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import '../styles/globals.css';
+import Footer from '../components/Footer';
+
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
+    <Footer />
     </>
   );
 }
