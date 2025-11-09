39400fa1bdc631638e46ee1ea4317ecb69521a16
storeskhaliil <storeskhaliil@gmail.com>
2025-11-09 20:23:57 +0200

    Add header links to the footer component

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,23 @@
 import styles from '../styles/Footer.module.css';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <p>
-        © {new Date().getFullYear()} CrtMenu | صُمم بواسطة{" "}
+        © {new Date().getFullYear()} CRTMenu | صُمم بواسطة{" "}
         <a href="https://khaliil.com" target="_blank" rel="noreferrer">
           KHALIIL
         </a>
       </p>
     </footer>
+
+      <header className={styles.header}>
+        <Link href="/contact">تواصل معي</Link>
+        <Link href="/">
+          <img src="/logo.png" alt="Logo" id="logo-img" />
+        </Link>
+        <Link href="/about">تعرف علي</Link>
+      </header>
   );
 }
