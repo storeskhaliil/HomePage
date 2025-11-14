3772177c083fcd4c231f510a904869dc580ef118
storeskhaliil <storeskhaliil@gmail.com>
2025-11-14 18:26:38 +0200

    Add Maintenance page with centered message

---
+++ pages/maintenance.js
@@ -0,0 +1,16 @@
+export default function Maintenance() {
+  return (
+    <div style={{
+      display:'flex',
+      justifyContent:'center',
+      alignItems:'center',
+      height:'100vh',
+      flexDirection:'column',
+      fontFamily:'sans-serif',
+      textAlign:'center'
+    }}>
+      <h1>الموقع مغلق مؤقتًا</h1>
+      <p>سنعود قريبًا!</p>
+    </div>
+  )
+}
