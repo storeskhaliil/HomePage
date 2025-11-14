f8e7491f61da212eb1b6b98ccd65834e624ea082
storeskhaliil <storeskhaliil@gmail.com>
2025-11-14 11:51:40 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: '/tem1',
+        destination: '/tem1.html',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
